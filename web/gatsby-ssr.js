e17ad10471768b03181e3482bbddbad7aab7971d
tomwrankmore <35687163+tomwrankmore@users.noreply.github.com>
2023-04-25 12:50:42 +0100

    ..

--- web/gatsby-ssr.js
+++ web/gatsby-ssr.js
@@ -5,3 +5,65 @@
  */
 
 // You can delete this file if you're not using it
+import * as React from "react"
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <link
+      rel="preload"
+      href="/fonts/NeueMontreal-Regular.otf"
+      as="font"
+      type="font/woff2"
+      crossOrigin="anonymous"
+      key="interFont"
+    />,
+    <link
+      rel="preload"
+      href="/fonts/NeueMontreal-Light.otf"
+      as="font"
+      type="font/woff2"
+      crossOrigin="anonymous"
+      key="interFont"
+    />,
+    <link
+      rel="preload"
+      href="/fonts/NeueMontreal-LightItalic.otf"
+      as="font"
+      type="font/woff2"
+      crossOrigin="anonymous"
+      key="interFont"
+    />,
+    <link
+      rel="preload"
+      href="/fonts/NeueMontreal-Medium.otf"
+      as="font"
+      type="font/woff2"
+      crossOrigin="anonymous"
+      key="interFont"
+    />,
+    <link
+      rel="preload"
+      href="/fonts/NeueMontreal-MediumItalic.otf"
+      as="font"
+      type="font/woff2"
+      crossOrigin="anonymous"
+      key="interFont"
+    />,
+    <link
+      rel="preload"
+      href="/fonts/NeueMontreal-Bold.otf"
+      as="font"
+      type="font/woff2"
+      crossOrigin="anonymous"
+      key="interFont"
+    />,
+    <link
+      rel="preload"
+      href="/fonts/NeueMontreal-BoldItalic.otf"
+      as="font"
+      type="font/woff2"
+      crossOrigin="anonymous"
+      key="interFont"
+    />
+  ])
+}
--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "./header";
 
-import "../styles/layout.css";
 import * as styles from "./layout.module.css";
 
 const Layout = ({ children, onHideNav, onShowNav, showNav, siteTitle }) => (
